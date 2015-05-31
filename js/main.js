9205434731a84720c53fa5ab37cbf2c03fc71b9f
JadeTiger88 <jadetiger88@gmail.com>
2015-05-31 10:15:03 -0700

    Update to use js for mood teller

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function showMe() {
+	name = document.getElementById("name").value; 
+	mood = document.getElementById("mood").value; 
+	if (!name || !mood) {
+		alert("Please enter all field(s).");
+		return false;
+	}
+
+	if (mood == "happy") {
+		face = " :)";
+	} else if (mood == "sad") {
+		face = " :("
+	} else {
+		face = " :|"
+	}
+
+	moodString = name + " is " + mood + " today" + face;
+	result = document.getElementById("result");
+	result.innerHTML = moodString;
+}
